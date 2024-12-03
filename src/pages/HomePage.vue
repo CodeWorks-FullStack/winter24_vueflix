03d43897c1ae79a2e5053cfcb28cc81b2ed89d71
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-03 10:04:04 -0700

    got movies with onMounted

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,37 +1,34 @@
 <script setup>
+import { moviesService } from '@/services/MoviesService.js';
+import { logger } from '@/utils/Logger.js';
+import Pop from '@/utils/Pop.js';
+import { onMounted } from 'vue';
+
+// NOTE lifecycle hook
+// NOTE onMounted will execute a function whenever this component is visible
+onMounted(() => {
+  logger.log('On mounted is running!')
+  discoverMovies()
+})
+
+// NOTE will run every time this component is no longer visible
+// onUnmounted(() => {
+//   console.log('On unMounted is running!');
+// })
+
+async function discoverMovies() {
+  try {
+    await moviesService.discoverMovies()
+  } catch (error) {
+    logger.error('[DISCOVER MOVIES]', error)
+    Pop.error(error)
+  }
+}
 
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
-  </div>
+  <p>Sup</p>
 </template>
 
-<style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
-}
-</style>
+<style scoped lang="scss"></style>
