207822f83fde479c58b106a54c01146636f815a6
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-03 10:34:05 -0700

    saving movies in appstate

--- src/services/MoviesService.js
+++ src/services/MoviesService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { movieApi } from "./AxiosService.js"
+import { Movie } from "@/models/Movie.js"
+import { AppState } from "@/AppState.js"
 
 class MoviesService {
   async discoverMovies() {
     const response = await movieApi.get('discover/movie')
     logger.log('DISCOVERED MOVIES 🎥', response.data)
+    const movies = response.data.results.map(moviePOJO => new Movie(moviePOJO))
+    AppState.movies = movies
   }
 }
 
